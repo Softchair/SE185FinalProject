082ecb1216f3f88e4687df17cee322ac8b38aab3
Softchair <24703877+Softchair@users.noreply.github.com>
2022-04-14 20:36:37 -0500

    Add files via upload

---
+++ FinalProjectTemplate.c
@@ -0,0 +1,39 @@
+//SE 185: Final Project Template//
+/////////////////////////
+/* 
+Team xx (please insert your team number instead of xx)
+Team member 1 "Name" | "Percentage of Contribution to The Project"
+Team member 2 "Name" | "Percentage of Contribution to The Project"
+Team member 3 "Name" | "Percentage of Contribution to The Project"
+Team member 4 "Name" | "Percentage of Contribution to The Project"
+*/
+
+////////////////////
+#include<stdio.h>//
+//add more here////
+
+//////////////////////
+//Struct Definition//
+////////////////////
+
+
+/////////////////////////////////////
+//User Defined Functions Prototype//
+//List prototypes here and define//
+//tehm below the main function////
+/////////////////////////////////
+
+int main(){
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+///////////////////////////////////////
+//User Defined Functions' Definition//
+/////////////////////////////////////
