4c696c4d9ff7ec51b6c001698c574b0216c859ec
KadenWingert1 <swiperkcw@gmail.com>
2022-04-17 21:57:00 -0500

    UI
    Added User interface that the user can read/respond to

--- FinalProjectTemplate.c
+++ FinalProjectTemplate.c
@@ -9,7 +9,9 @@ Team member 4 Camden Fergen | "Percentage of Contribution to The Project"
 */
 
 ////////////////////
-#include<stdio.h>//
+#include<stdio.h>
+#include<stdlib.h>
+
 //add more here////
 
 //////////////////////
@@ -25,10 +27,16 @@ Team member 4 Camden Fergen | "Percentage of Contribution to The Project"
 
 int main(){
 	
-	
-	//test p2
-	
-	
+	char response;
+	int minimumLength;
+	printf("Hello, this is a typing game. You will need to type the words as they appear/n");
+	printf("Would you like to play or add a word?/n");
+	printf("You currently have 58 words that can be used. (p/w)");
+	scanf("%c", &response);
+	//if they say do want to endter a word:
+	printf("What would you like you minimum word length to be?");
+	scanf("%d", &minmumLength);
+	//go into conditional that adds a word if they choose to add a word
 	
 	
 	return 0;
