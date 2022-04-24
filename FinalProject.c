50575013a8b8c4b6fe77d543da570bc558b9e80a
Softchair <24703877+Softchair@users.noreply.github.com>
2022-04-23 20:57:47 -0500

    Code cleanup and add
    Cleaned up and commented all the code. Also I added a bunch of functions and getting words as well as adding, and then also getting ready to play the game with only getting words over a certain length

--- FinalProject.c
+++ FinalProject.c
@@ -1,5 +1,3 @@
-//SE 185: Final Project Template//
-/////////////////////////
 /* 
 Team xx (please insert your team number instead of xx)
 Team member 1 Kaden Wingert | "Percentage of Contribution to The Project"
@@ -17,147 +15,262 @@ Team member 4 Camden Fergen | "Percentage of Contribution to The Project"
 #include <unistd.h>
 #include<stdlib.h>
 #include<string.h>
+////////////////////
 
-//Constant variables////
-#define COLUMNS 30
-#define ROWS 31
+/* Constant variables */
+#define COLUMNS 50
+#define ROWS 50
 #define FLOOR '_'
 #define WALL '|'
 #define EMPTY_SPACE ' '
+#define WORDLENGTH 20
 
-//2D character array which the maze is mapped into 
+/////////////////////////////////
+/* Function initialization */
+
+/* Iterates over a file to read all the words from
+   it and then returns the num of words avalible to use */
+int readWords(char* fileName, int minLength);
+
+/* Iterates over the BOARD[][] variable and then
+   uses drawChar to print it to the screen */
+void drawBoard(void);
+
+/* This function draws a character to the screen
+   Using x and y */
+void drawChar(int x, int y, char use);
+
+/* Generates an empty board to print to the screen
+   using WALL, FLOOR, and EMPTY_SPACE assigned to
+   the BOARD[][] variable */
+void generateBoard();
+
+/* End function initialization */
+/////////////////////////////////
+
+/* Structure to hold the words during the game */
+typedef struct {
+	char word[WORDLENGTH];
+	int row;
+	int col;
+} wordStruct;
+
+/////////////////////////////////
+/* Global variables */
+
+/* Variable for the game board */
 char BOARD[ROWS][COLUMNS];
-//Keeps track of the remaining number of words the user can add from the file to the game
-int useWords = 58;
-char word[];
 
-//Struct Definition
+/* Varible of game words */
+//Fun fact, there are 1000 words in the word list already...
+wordStruct gameWords[2000];
 
+/* Varible to hold number of words */
+int numWords = 0;
+int numGameWords = 0;
 
+/* End global variables */
+/////////////////////////////////
 
+/* Main function to run the game */
+int main() {
 
-/*
-	USER-DEFINED FUNCTIONS
-*/
+	/* Min length of a word */
+	int minWordLen = 0;
 
-//Function draws the characters on the board
-void draw_board(void);
-//Draws character use to the screen at position x,y 
-void draw_character(int x, int y, char use);
-//Determines which spots on the board should have a character '-' or '|'
-void generate_board();
+	/* Game setup */
 
+	//Reads from wordList file and calcs numWords
+	char wordsFile[20] = "wordList.txt";
+	FILE* fp = fopen(wordsFile, "r");
 
+	//Checks to make sure the file exists
+		if(fp == NULL) {
+			printf("The file could not be opened");
+		return -1;
+	}
 
+	//Reads num of words in file
+	char* p;
+	while(!feof(fp)) {
+		char temp[WORDLENGTH];
+		p = fgets(temp, WORDLENGTH, fp);
 
+		if(p != NULL) {
+			numWords++;
+		}
+	}
+	fclose(fp);
 
-int main(){
-	char response;
-	int minmumLength;
-	int wordCount = 0;
-	 /* 	Setup screen for Ncurses
-        The initscr function is used to setup the Ncurses environment
-        The refreash function needs to be called to refresh the outputs
-        to the screen */
-    initscr();
-    refresh();
+	/* End game setup */
 	
-	//These 3 lines are going to be moved into the conditional; they are just here for testing the board...board does print correctly but we can change the dimensions if we want
+
+	//Generate board, move later
+    // initscr();
+    // refresh();
 	// generate_board();
 	// draw_board();
 	// refresh();
-	
-	mvprintw(ROWS,COLUMNS+3, "Hello, this is a typing game. You will need to type the words as they appear\n");
-	mvprintw(ROWS,COLUMNS+3,"Would you like to play or add a word?\n");
-	mvprintw(ROWS,COLUMNS+3,"You currently have %d words that can be used.\nType 'p' to play, 'w' to add a word or any other button to exit: ", useWords);
+
+	//Variable to hold user response
+	char response;
+
+	//Staring messages
+	printf("Hello, this is a typing game. You will need to type the words as they appear\n");
+	printf("Would you like to play or add a word?\n");
+	printf("You currently have %d words that can be used.\n", numWords);
+	printf("Type 'p' to play, 'w' to add a word or any other button to exit: ");
 	scanf("%c", &response);
-	//if they do want to enter a word:
-	if(response == 'w')
-	{	//Continues to ask the user to enter words until they type something other than 'p'
-	do
-	{
-	printf("What word would you like to add?\n");
-	scanf("%s", &word);
-	//write into file with contents of new word
-	wordCount++;
-	printf("There are now %d words. Would you like to play or add another word? (p/w)\n", wordCount);
-	scanf("%c" &response);
-	} while(response == 'w');
-	//Needs to read into the file, get the corresponding word from the file, and write this word into a new/empty file
 	
-	
-	}
-	else if(response == 'p'){
+	/* Enters conditional if the user wants to add a word 
+	   Uses do while loop to add words till user is done
+	   adding words. */
+	if(response == 'w') {
+		FILE* fp = fopen(wordsFile, "a");
+
+		//Checks to make sure the file exists
+		if(fp == NULL) {
+			printf("The file could not be opened");
+		return -1;
+		}
+
+		while(response == 'w') {
+			//Varible to hold word to be added
+			char word[WORDLENGTH];
+
+			printf("What word would you like to add?\n");
+			scanf("%s", word);
+
+			//Write into file with contents of new word
+			fprintf(fp, "\n%s", word);
+
+			//Increment the number of words
+			numWords++;
+
+			//Ask the user if they want to add any more words or play the game
+			printf("There are now %d words. Would you like to play or add another word? (p/w): ", numWords);
+			scanf(" %c", &response);
+		}
+
+	} else if(response == 'p') {
+
 		printf("What would you like you minimum word length to be? ");
-		scanf("%d", &minmumLength);
-		//Needs to read into the file, and pick random words that have a length >= the number that the user chose
-	}
-	else{
-		exit(1);
+		scanf("%d", &minWordLen);
+
+		//Uses readWords function to popualte words for game
+		numGameWords = readWords(wordsFile, minWordLen);
+
+	} else {
+		return 0;
 	}
 
+	for(int i = 0; i < numGameWords; i++) {
+		printf("%s\n", gameWords[i].word);
+	}
+
+	//Cleanup board
 	endwin();
 	return 0;
 }
 
-///////////////////////////////////////
+//////////////////////////////////////
 //User Defined Functions' Definition//
-/////////////////////////////////////
+//////////////////////////////////////
 
 
-//I will edit this up a bit to make it work, this is copy pasted from lab 9
-// int read_words(char* WL[MAXWORDS], char* file_name) {
-	// int numread = 0;
-	// char line[WORDLENGTH];
-	// char *p;
-	// FILE* fp = fopen(file_name, "r");
-	// while (!feof(fp)) {
-		// p = fgets(line, WORDLENGTH, fp);
-		// if (p != NULL) 
-		// {
-			// trimws(line);
-			// WL[numread] = (char *)malloc(strlen(line) + 1);
-			// strcpy(WL[numread], line);
-			// numread++;
-		// }
-	// }
-	// fclose(fp);
-	// return numread;
-// }
+/* Iterates over a file to read all the words from
+   it and then returns the num of words in file */
 
-void draw_board(void){
-	for(int i = 0; i< ROWS;++i){
-		for(int j= 0; j< COLUMNS; ++j){
-		draw_character(i,j,BOARD[i][j]);	
+//RE DO THIS TO ONLY SCAN CERTAIN WORDS
+int readWords(char* fileName, int minLength) {
+	int numread = 0;
+	char line[WORDLENGTH];
+	char *p;
+
+	//Opens the file through filename
+	FILE* fp = fopen(fileName, "r");
+
+	//Checks to make sure the file exists
+	if(fp == NULL) {
+		printf("The file could not be opened");
+		return -1;
+	}
+
+	//While its not the end of file (feof)
+	while (!feof(fp)) {
+
+		//Gets the first word
+		p = fgets(line, WORDLENGTH, fp);
+
+		//If there is a word
+		//Note, its minLength + 1 to account for null char and new line
+		if (p != NULL && strlen(p) > minLength + 1) {
+			//trimws(line); This was a function from lab 9? didnt add it here yet
+
+			wordStruct tempWord;
+
+			//Copies data over to temp word to be added to array
+			strncpy(tempWord.word, line, 20);
+			tempWord.row = 0; tempWord.col = 0;
+
+			gameWords[numread] = tempWord;
+			numread++;
 		}
 	}
+
+	//Closes the file
+	fclose(fp);
+	return numread;
 }
 
-void draw_character(int x, int y, char use)
-{
-    mvaddch(x,y,use);
+/* This function draws a character to the screen
+   Using x and y */
+void drawChar(int x, int y, char use) {
+    mvaddch(x, y, use);
     refresh();
 }
 
-void generate_board(){
-		for(int i = 0; i< ROWS;++i){
-			for(int j= 0; j< COLUMNS; ++j){
-					//causes the board to have FLOOR on the top and bottom
-					if( i == 0 || i == ROWS-1 || i == ROWS-2){
-				BOARD[i][j] = FLOOR;
-			}//causes the board to have WALL on the left and right
-					else if (j == 0 || j == COLUMNS-1){
-				BOARD[i][j] = WALL;
-			}
-			else{
-				BOARD[i][j] = EMPTY_SPACE;
-			}
-		}
+///////////////////////////////////////////////////////
+/* Functions to create and draw board the the screen */
+///////////////////////////////////////////////////////
 
+/* Iterates over the BOARD[][] variable and then
+   uses drawChar to print it to the screen */
+void drawBoard(void) {
+	for(int irow = 0; irow < ROWS; irow++){
+		for(int jcol = 0; jcol < COLUMNS; jcol++){
+		drawChar(irow, jcol, BOARD[irow][jcol]);	
+		}
 	}
-	
 }
 
-//Paste in the function from lab 9 that stores the values of each word so that the words can each be removed
+/* Generates an empty board to print to the screen
+   using WALL, FLOOR, and EMPTY_SPACE assigned to
+   the BOARD[][] variable */
+void generateBoard() {
 
+	//Iterates over the rows
+	for(int irow = 0; irow < ROWS; irow++) {
 
+		//Iterates over the columns
+		for(int jcol = 0; jcol < COLUMNS; jcol++) {
+
+			/* This condition checks if its the first or last two rows
+		       then prints a line of FLOORS */
+			if(irow == 0 || irow == ROWS - 1 || irow == ROWS - 2) {
+				BOARD[irow][jcol] = FLOOR;
+			
+			/* This condition places walls on left and right */
+			} else if (jcol == 0 || jcol == COLUMNS - 1) {
+				BOARD[irow][jcol] = WALL;
+
+			/* Else places everything as empty space */
+			} else {
+				BOARD[irow][jcol] = EMPTY_SPACE;
+			}
+
+		//end for
+		}
+	//end for
+	}
+}
